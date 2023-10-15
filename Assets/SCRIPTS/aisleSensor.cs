1ec21c5c1944f56d773bd6be1dd41d6cfb42af7a
jamnk <pancakes3825@gmail.com>
2023-10-15 15:09:03 -0400

    aisle enemy

---
+++ Assets/SCRIPTS/aisleSensor.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class aisleSensor : MonoBehaviour
+{
+    public shopper shoppa;
+    public bool playerDetected = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            shoppa.agressive = true;
+            shoppa.speed = 4;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            shoppa.agressive = false;
+            shoppa.speed = 1;
+        }
+        if(other.tag == "shopper")
+        {
+            shoppa.speed = -shoppa.speed;
+        }
+    }
+
+}
---
+++ Assets/SCRIPTS/shopper.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class shopper : MonoBehaviour
+{
+    private Vector3 target;
+    public player playr;
+    public float speed;
+    public SpriteRenderer sprite;
+    public Vector3 aisle;
+    public bool agressive = false;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (playr != null)
+        {
+            if (agressive)
+            {
+                target = playr.transform.position;
+                transform.LookAt(target);
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+                sprite.transform.rotation = Quaternion.Euler(0, 0, 0);
+            }
+            else
+            {
+                transform.Translate(Vector3.forward * Time.deltaTime * speed);
+            }
+
+            if (target.x - transform.position.x >= 0)
+            {
+                sprite.flipX = true;
+            }
+            if (target.x - transform.position.x < 0)
+            {
+                sprite.flipX = false;
+            }
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(playr.gameObject);
+        }
+    }
+}
